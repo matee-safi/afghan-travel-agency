8033c65994ea66acf31a54aec9ce92bdb023daac
Matee <matiullahsafibbc@gmail.com>
2023-10-28 17:41:44 +0430

    add useParam functionality

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['cdn.enjoytravel.com', 'i.ibb.co', 'uscis.gov', 'www.uscis.gov'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from 'public/logo.png';
 
 const Nav = () => {
   return (
     <nav className="nav sticky top-0">
-      <a href="/">
+      <Link href="/">
         <div className="flex items-center h-12 justify-center">
           <Image src={logo} width={25} height={25} alt="logo" />
           <h1 className="logo-text h-7 pl-px text-2xl">Afghan Travel Agency</h1>
         </div>
-      </a>
+      </Link>
     </nav>
   );
 }
