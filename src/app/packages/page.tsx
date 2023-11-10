e2250050a9e93bee764a4e726cb852da7a015785
matee-safi <matiullahsafibbc@gmail.com>
2023-11-10 20:35:37 +0430

    fix minor bugs

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -23,12 +23,10 @@ export default function Packages() {
 
   const openPopup = (index: any) => {
     setSelectedPackage(index);
-    document.body.classList.add('no-scroll'); // Add the CSS class to disable scrolling
   };
   
   const closePopup = () => {
     setSelectedPackage(null);
-    document.body.classList.remove('no-scroll'); // Remove the CSS class to enable scrolling
   };
 
   const handlePopupClick = (e: React.MouseEvent) => {
